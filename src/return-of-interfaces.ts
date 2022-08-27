4655e9ab83ce40a1fcdb7670b2d09df54330456f
yossi509311 <yossi509311@gmail.com>
2022-08-27 02:58:00 +0000

    class implement3

---
+++ src/return-of-interfaces.ts
@@ -0,0 +1,28 @@
+export {};
+
+class MahoTsukai {}
+
+class Souryo {}
+
+class Taro extends MahoTsukai {}
+
+interface Kenja {
+  ionazun(): void;
+}
+
+interface Senshi {
+  kougeki(): void;
+}
+
+class Jiro implements Kenja, Senshi {
+  ionazun(): void {
+    console.log('ionazun');
+  }
+  kougeki(): void {
+    console.log('kougeki');
+  }
+}
+
+const jiro = new Jiro();
+jiro.ionazun();
+jiro.kougeki();
